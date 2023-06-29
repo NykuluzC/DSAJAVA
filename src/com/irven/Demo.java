7117c3f86c6ddb8dfc7a25850c1d1b2b3a45246d
Nykuluz <carlmillanes3@gmail.com>
2023-06-29 17:14:47 +0800

    Package Initialization and samples

---
+++ src/com/irven/Demo.java
@@ -0,0 +1,7 @@
+package com.irven;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("I am Irven");
+    }
+}
--- src/com/nykuluz/Main.java
+++ src/com/nykuluz/Main.java
@@ -1,5 +1,7 @@
+package com.nykuluz;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("I am Nykuluz");
     }
 }
