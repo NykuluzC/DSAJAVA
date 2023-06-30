d5a4f1aaf32a8bb7abeed8330ad7f964948f3d18
Nykuluz <carlmillanes3@gmail.com>
2023-07-01 07:48:06 +0800

    Changed class name file

--- src/com/conditionalsandloops/Main.java
+++
@@ -1,4 +0,0 @@
-package com.conditionalsandloops;
-
-public class Main {
-}
---
+++ src/com/conditionalsandloops/Salary.java
@@ -0,0 +1,8 @@
+package com.conditionalsandloops;
+
+public class Salary {
+    public static void main(String[] args) {
+
+        if()
+    }
+}
