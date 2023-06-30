256dfdd82223b4366d21072a56bde8a08b6c07fb
Nykuluz <carlmillanes3@gmail.com>
2023-06-30 08:35:46 +0800

    Mark Generator

--- src/com/irven/Demo.java
+++ src/com/irven/Demo.java
@@ -5,9 +5,27 @@ import java.util.Scanner;
 public class Demo {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Please enter some integer value: ");
+
+        //Input 1
+        System.out.print("Please enter your rollno value: ");
         int rollno = input.nextInt();
-        System.out.println("Your roll number is: " + rollno);
+        input.nextLine();
+
+        //Input 2
+        System.out.print("Please enter your name: ");
+        String name = input.nextLine();
+
+        System.out.print("Please enter your marks: ");
+        float marks = input.nextFloat();
+        input.nextLine();
+
+        System.out.print("Subject Teacher: ");
+        String teacher = input.nextLine();
+
+        System.out.println("Your name is " +
+                name + " and your rollno is: " + rollno + "."
+                + "\nThat corresponds to a mark of: " + marks + "."
+                + "\nThe teacher of this subject is: " + teacher);
     }
 }
 
