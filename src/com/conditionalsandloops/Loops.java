d6903fb807aca7deb2ebfc9162ad4bd120129e8f
Nykuluz <carlmillanes3@gmail.com>
2023-07-01 08:09:44 +0800

    Comments for for loop

--- src/com/conditionalsandloops/Loops.java
+++ src/com/conditionalsandloops/Loops.java
@@ -14,8 +14,13 @@ public class Loops {
         System.out.println(5);
         */
 
+        //Print Number 1 to 5
         for(int i = 1; i <= 5; i++){
             System.out.println(i);
         }
-    }
+        //Print 5 Hello World
+        for(int z = 0; z < 5; z++){
+            System.out.println("Hello World!");
+        }
+    }1
 }
