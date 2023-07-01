eb99ab6c9449749643e2c9accae71f4ceb9fd1c8
Nykuluz <carlmillanes3@gmail.com>
2023-07-01 08:03:07 +0800

    Loops Example

---
+++ src/com/conditionalsandloops/Loops.java
@@ -0,0 +1,21 @@
+package com.conditionalsandloops;
+
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        /*
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
+        System.out.println(5);
+        */
+
+        for(int i = 1; i <= 5; i++){
+            System.out.println(i);
+        }
+    }
+}
--- src/com/conditionalsandloops/Salary.java
+++ src/com/conditionalsandloops/Salary.java
@@ -1,8 +1,21 @@
 package com.conditionalsandloops;
 
+import java.util.Scanner;
+
 public class Salary {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-        if()
+        System.out.print("Input Salary: ");
+        int salary = input.nextInt();
+        if(salary <= 10000)
+        {
+            salary += 1000;
+        } else if(salary <= 20000){
+            salary += 2000;
+        } else {
+            salary += 3000;
+        }
+        System.out.println(salary);
     }
 }
