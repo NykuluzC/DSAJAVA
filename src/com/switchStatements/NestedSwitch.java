776dcd32bf602aaac2d651396bd1dce73bd7c5a9
Nykuluz <carlmillanes3@gmail.com>
2023-07-06 11:38:06 +0800

    Nested Switch Statements

---
+++ src/com/switchStatements/NestedSwitch.java
@@ -0,0 +1,38 @@
+package com.switchStatements;
+
+import java.util.Scanner;
+import java.util.SortedMap;
+
+public class NestedSwitch {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Employee Number: ");
+        int empID = in.nextInt();
+        System.out.print("Department: ");
+        String department = in.next();
+
+        switch (empID){
+            case 1:
+                switch (department.toLowerCase()){
+                    case "it":
+                        System.out.println("***IT Department***");
+                        break;
+                    case "management":
+                        System.out.println("***Management Department***");
+                        break;
+                    default:
+                        System.out.println("***No department or unassigned***");
+                }
+                System.out.println("Carl Irven Nykuluz Millanes");
+                break;
+            case 2:
+                System.out.println("Ralph Rivera");
+                break;
+            case 3:
+                System.out.println("Jose Hinolan");
+                break;
+            default:
+                System.out.println("Enter correct EmpID");
+        }
+    }
+}
