75b31446fd1db2acb73c5be58af10836e53a73a7
Nykuluz <carlmillanes3@gmail.com>
2023-07-10 08:23:48 +0800

    Greeting method example

---
+++ src/com/methods/Greeting.java
@@ -0,0 +1,11 @@
+package com.methods;
+
+public class Greeting {
+    public static void main(String[] args) {
+        Hello("Nykuluz");
+    }
+
+    static void Hello(String name){
+        System.out.println("Good morning " + name);
+    }
+}
