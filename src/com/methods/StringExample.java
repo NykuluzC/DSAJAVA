69e56aefeb97878abe491d0338a87e1adbf94dc6
Nykuluz <carlmillanes3@gmail.com>
2023-07-10 08:37:17 +0800

    Full method implementation with return type and parameters included

---
+++ src/com/methods/StringExample.java
@@ -0,0 +1,12 @@
+package com.methods;
+
+public class StringExample {
+    public static void main(String[] args) {
+        String output = greet();
+        System.out.println(output);
+    }
+
+    static String greet(){
+        return "Hello";
+    }
+}
--- src/com/methods/Sum.java
+++ src/com/methods/Sum.java
@@ -4,29 +4,35 @@ import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
-        sum();
-        int ans = sum2();
-        System.out.println(ans);
+        int result = sum3(3,4);
+        System.out.println(result);
     }
-
+    //sum with integer return type and 2 integer parameters
+    static int sum3(int num1, int num2){
+        Scanner in = new Scanner(System.in);
+        return num1 + num2;
+    }
+    //sum with integer return type
     static int sum2(){
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter number 1: ");
+        System.out.print("Enter number 1: ");
         int num1 = in.nextInt();
-        System.out.println("Enter number 2: ");
+        System.out.print("Enter number 2: ");
         int num2 = in.nextInt();
         int sum = num1 + num2;
         return sum;
 
     }
+
+    //no return type
     static void sum() {
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter number 1: ");
+        System.out.print("Enter number 1: ");
         int num1 = in.nextInt();
-        System.out.println("Enter number 2: ");
+        System.out.print("Enter number 2: ");
         int num2 = in.nextInt();
         int sum = num1 + num2;
-        System.out.println("The sum = " + sum);
+        System.out.print("The sum = " + sum);
 
     }
 }
