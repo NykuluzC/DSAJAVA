0a63ee3b3fb8ba87ab2212ec438caa44f53ca524
Nykuluz <carlmillanes3@gmail.com>
2023-07-10 08:26:58 +0800

    Integer and void method implementation and knowing its difference

--- src/com/methods/Sum.java
+++ src/com/methods/Sum.java
@@ -5,9 +5,20 @@ import java.util.Scanner;
 public class Sum {
     public static void main(String[] args) {
         sum();
-        sum();
+        int ans = sum2();
+        System.out.println(ans);
     }
 
+    static int sum2(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter number 1: ");
+        int num1 = in.nextInt();
+        System.out.println("Enter number 2: ");
+        int num2 = in.nextInt();
+        int sum = num1 + num2;
+        return sum;
+
+    }
     static void sum() {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter number 1: ");
