d6bcb28aacfae71aea1cfcef8bc6ab8c69e705b7
Nykuluz <carlmillanes3@gmail.com>
2023-07-11 14:28:54 +0800

    String Methods Implementation with parameters and return type application

--- src/com/methods/StringExample.java
+++ src/com/methods/StringExample.java
@@ -1,9 +1,25 @@
 package com.methods;
 
+import java.util.Scanner;
+
 public class StringExample {
     public static void main(String[] args) {
         String output = greet();
         System.out.println(output);
+
+
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter Your Greeting : ");
+        String greeting = in.nextLine();
+
+        String personalized = myGreet(greeting);
+        System.out.println(personalized);
+    }
+
+    static String myGreet(String greet) {
+        String message = "Hello, " + greet;
+        return message;
     }
 
     static String greet(){
