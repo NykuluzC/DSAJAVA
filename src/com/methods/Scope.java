8cfb2bc53bd8d29eea0485a1ec36ae5696fedd8f
Nykuluz <carlmillanes3@gmail.com>
2023-07-13 18:55:21 +0800

    Function Scoping

---
+++ src/com/methods/Scope.java
@@ -0,0 +1,15 @@
+package com.methods;
+
+public class Scope {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println(num);
+        System.out.println(marks);
+    }
+
+    static void random(int marks) {
+        int num = 67;
+        System.out.println(num);
+    }
+}
