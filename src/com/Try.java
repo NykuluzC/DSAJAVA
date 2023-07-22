bfd1f1364030c26384b279071e960d7e7199e2ba
Nykuluz <carlmillanes3@gmail.com>
2023-07-22 11:55:54 +0800

    Added Practice

---
+++ src/com/Try.java
@@ -0,0 +1,15 @@
+package com;
+
+public class Try {
+    public static void main(String[] args) {
+        int[] x = {4,5,6,7};
+        int[] y = {10,11,12,13};
+
+        for(int i = 0; i < 4; System.out.println(x[i++]){
+            for(int z = 0; z < 4; z++){
+                System.out.println(y[z]);
+            }
+            System.out.println(x[i]);
+        }
+    }
+}
